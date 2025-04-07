eabe487a611bcacb43628d0cafaeb98106f74c1e
Adhi0068 <166810290+Adhi0068@users.noreply.github.com>
2025-04-07 18:24:12 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/demo/app/Controller/HelloWorldController.java
+++ src/main/java/com/example/demo/app/Controller/HelloWorldController.java
@@ -6,15 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    int a = o;
+    
 
     @GetMapping("/message")
     public int getMessage(){
-        for (int i = 0 ; i < 67 ; i++){
-            i = i + 10;
-            int a = i ;
-        }
-        return a ;
+        int z = 9 + 15;
+        return z ;
     }
     
 }
